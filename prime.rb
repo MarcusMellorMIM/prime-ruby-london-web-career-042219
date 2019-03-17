25328cfde1c908e2ab7d8cb678dc347422fdab89
Marcus Mellor <marcus@mellor.com>
2019-03-17 18:23:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,24 +7,3 @@ def prime?( value )
   end
   return_value
 end
-
-
-def prime?old( value )
-  test_array=(2...value).to_a
-  test_array  
-  counter=0
-  return_value=true
-  if value>1
-    while counter < test_array.count
-      if value % test_array[counter]==0 
-        return_value=false
-        break
-      end
-      counter+=1
-    end
-  else
-    return_value=false
-  end
-  return_value
-end
-
