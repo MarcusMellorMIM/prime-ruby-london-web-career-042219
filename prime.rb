73c9283f1bd4db36d74537ed92fe669298ad7d9f
Marcus Mellor <marcus@mellor.com>
2019-03-17 17:43:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,13 @@
 def prime?( value )
   test_array=(1...value).to_a
   test_array  
+  counter=0
+  return_value=true
+  while counter < test_array.count
+    if value % test_array[counter]==log10
+      return_value=false 
+      break
+    end
+    counter+=1
+  end
 end
