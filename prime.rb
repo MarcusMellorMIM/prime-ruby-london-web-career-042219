211e3e8566f3380613b132283cf17afe2c49216d
Marcus Mellor <marcus@mellor.com>
2019-03-17 18:08:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?( value )
   test_array(2...value).to_a
-  return_value=test_array.index?{|x| value%x==0}
-  !return_value
+  return_value=test_array.index{|x| value%x==0}
+  return_value
 end
 
 def prime?old( value )
