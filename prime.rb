88f33ad33940eb252f55f4b647e4fb0c192387c1
Marcus Mellor <marcus@mellor.com>
2019-03-17 17:47:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,13 @@ def prime?( value )
   test_array=(2...value).to_a
   test_array  
   counter=0
-  return_value=true
+  return_value=false
   while counter < test_array.count
     if value % test_array[counter]==0 
-      return_value=false 
       break
     end
     counter+=1
+    return_value=true
   end
   return_value
 end
